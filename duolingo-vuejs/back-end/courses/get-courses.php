c774d6e926a5a1a652eb9d5186f6f799710a0986
kieutrongloc <kieutrongloc1298@gmail.com>
2023-03-29 03:07:42 +0700

    xx

--- duolingo-vuejs/back-end/courses/get-courses.php
+++ duolingo-vuejs/back-end/courses/get-courses.php
@@ -1,6 +1,8 @@
 <?php
 header('Cache-Control: no-cache');
-header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Authorization');
+header('Access-Control-Allow-Methods: *');
 // database credentials
 $host = "duolingo.cyfy53duasbw.ap-southeast-2.rds.amazonaws.com";
 $dbname = "duolingo";
@@ -16,11 +18,18 @@ try {
 }
 
 // prepare and execute a SELECT statement
-$stmt = $dbh->prepare("SELECT id, code, title FROM courses");
+$stmt = $dbh->prepare("SELECT id, code, image, title FROM courses");
 $stmt->execute();
 
 // fetch the results as an associative array
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// encode the image data in base64 format
+foreach ($results as &$result) {
+  if ($result['image']) {
+    $result['image'] = base64_encode($result['image']);
+  }
+}
 
 // output the results as a JSON object
 header('Content-Type: application/json');
