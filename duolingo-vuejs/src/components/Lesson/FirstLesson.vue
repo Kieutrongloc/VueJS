5a7fe49c265c9cbdc0779ddbaa6624271ba0b41c
kieutrongloc <kieutrongloc1298@gmail.com>
2023-03-28 14:36:21 +0700

    xx

--- duolingo-vuejs/src/components/Lesson/FirstLesson.vue
+++ duolingo-vuejs/src/components/Lesson/FirstLesson.vue
@@ -24,33 +24,31 @@
 
 <template>
   <div>
-    <ul>
-      <li v-for="post in posts" :key="post.id">
-        <h2>{{ post.code }}</h2>
-        <p>{{ post.title }}</p>
-      </li>
-    </ul>
   </div>
 </template>
-
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
       posts: [],
     };
   },
-  async mounted() {
-    try {
-      const response = await fetch('./../../../back-end/courses/get-courses.php');
-      const data = await response.json();
-      this.posts = data;
-    } catch (error) {
-      console.error('Error fetching posts:', error);
-    }
-    console.log(this.posts);
+  created() {
+    this.getCourses();
+  },
+  methods: {
+    async getCourses() {
+      try {
+        const response = await axios.get('http://localhost/www/AppleStore/Duolingo/get-courses.php');
+        this.posts = response.data;
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+      console.log(this.posts);
+    },
   },
 };
-
 </script>
 
